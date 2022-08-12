04ad824613bc656ff01f69e93e476d6e101b0f76
samiullah997 <samiullahk997@gmail.com>
2022-08-12 10:13:18 +0500

    Remove commented and useable code from the remove.js file

--- src/cruds/remove.js
+++ src/cruds/remove.js
@@ -1,9 +1,4 @@
 function removeItem(itemIndex, data) {
-  // const item = document.getElementById(index);
-  //   item.remove();
-  //   data = data.filter((list) => list.index !== index);
-  //   localStorage.setItem('NOTE',JSON.stringify(data));
-
   const item = document.getElementById(itemIndex);
   item.remove();
   data = data.filter((list) => list.index !== itemIndex);
